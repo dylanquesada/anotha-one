ad093b8daf98d51e540bbf57ad8c78b1dcc60921
Dylan Quesada <dquesada@Dylans-MacBook-Pro.local>
2019-08-22 21:20:19 -0500

    Updates

---
+++ precache-manifest.f94e57413112c80529a9474f71b75e9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71d7afb483d682fd4323c39a69378293",
+    "url": "/anotha-one/index.html"
+  },
+  {
+    "revision": "6ac6748456af187b6320",
+    "url": "/anotha-one/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "bc3ef3bbbec5dc6eb29f",
+    "url": "/anotha-one/static/js/2.0bbab059.chunk.js"
+  },
+  {
+    "revision": "6ac6748456af187b6320",
+    "url": "/anotha-one/static/js/main.e5508204.chunk.js"
+  },
+  {
+    "revision": "2fd5ab3f105c776aac45",
+    "url": "/anotha-one/static/js/runtime~main.0f3a2b7b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/anotha-one/static/media/logo.5d5d9eef.svg"
+  }
+]);
